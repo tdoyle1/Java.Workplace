fadc0e570d6cb55b9e5d6d98dbb54b678a44af54
ceb-student <CEB-Student@caddoschools.org>
2017-08-24 09:17:41 -0500

    First Program
    Started making a basic input output program in Java.

---
+++ LearningAboutStrings/src/basicInputOutput/controller/BasicIOController.java
@@ -0,0 +1,11 @@
+package basicInputOutput.controller;
+
+public class BasicIOController 
+{
+	public void printGreeting()
+	{
+		String firstName = "Trevor";
+		String lastName = "Doyle";
+		System.out.println(firstName + " " + lastName);
+	}
+}
